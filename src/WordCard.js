5a70c9ff525daaae0abd172fcea17bedb55dc99e
army1582 <mark.mk1582@gmail.com>
2018-09-14 03:53:51 +0700

    Handler as Props

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 export default class WordCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+    activationHandler = c => { console.log(`${c} has been activated.`) }
     render() {
         return (
           <div>
-    { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+    { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler}/>) }
           </div>
         );
       }
